8b792d5f1933870fd68d6d025117c5ca0b44d10a
User02001 <listeve319@gmail.com>
2025-08-29 21:57:10 +0800

    smth

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
  plugins: [react()],
- server: {
-  host: true,   // or '0.0.0.0' → allows LAN access
-  port: 5173,   // optional: set a fixed port
+ build: {
+  outDir: 'dist-obfuscated', // 👈 MUST be set
  },
 })
