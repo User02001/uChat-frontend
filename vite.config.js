d131218165bcfc37a22ac80315eef192cbe1b427
User02001 <listeve319@gmail.com>
2025-09-21 09:46:52 +0800

    Making calls work without error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- define: {
-  global: 'globalThis',
- },
+ plugins: [
+  react(),
+  nodePolyfills({
+   globals: {
+    Buffer: true,
+    global: true,
+    process: true,
+   },
+   protocolImports: true,
+  })
+ ],
  optimizeDeps: {
   include: ['simple-peer'],
  },
