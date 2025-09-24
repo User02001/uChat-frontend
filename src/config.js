af5c9faa766808557cfff7e06f17958957ba2d0c
User02001 <listeve319@gmail.com>
2025-09-24 21:08:51 +0800

    small update cdn V6

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const config = {
  development: {
   API_BASE_URL: 'https://192.168.1.48:5001',
-  SOCKET_URL: 'https://192.168.1.48:5001'
+  SOCKET_URL: 'https://192.168.1.48:5001',
+  CDN_BASE_URL: 'https://cdn.ufonic.xyz' // Same as API in dev
  },
  production: {
   API_BASE_URL: 'https://api.ufonic.xyz',
-  SOCKET_URL: 'https://api.ufonic.xyz'
+  SOCKET_URL: 'https://api.ufonic.xyz',
+  CDN_BASE_URL: 'https://cdn.ufonic.xyz' // CDN subdomain in prod
  }
 };
 
@@ -15,5 +17,6 @@ const environment = isDevelopment ? 'development' : 'production';
 
 export const API_BASE_URL = config[environment].API_BASE_URL;
 export const SOCKET_URL = config[environment].SOCKET_URL;
+export const CDN_BASE_URL = config[environment].CDN_BASE_URL;
 
 export default config[environment];
